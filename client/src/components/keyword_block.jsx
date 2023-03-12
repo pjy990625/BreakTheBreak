94ff7f15805d31b64c1305cffc8e814c606f4bfe
kohdc <kohdc1723@gmail.com>
2023-03-12 09:36:20 -0700

    remove unusing code

--- client/src/components/keyword_block.jsx
+++ client/src/components/keyword_block.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import "../index.css";
 
-const keyword_styles = {
-    display: "inline-block",
-    position: "relative",
-    background: "",
-    border: "1px solid black",
-    width: "fit-content",
-    margin: "5px",
-    padding: "1px 7px",
-}
-
 const KeywordBlock = (props) => {
 
     return (
