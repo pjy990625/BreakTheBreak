ffa30fbf01893bfeb64e686671c5a8b94cb52ab5
kohdc <kohdc1723@gmail.com>
2023-03-11 17:34:49 -0800

    add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Home />} />
         <Route path="/read/:id" element={<ReadPost />} />
         <Route path="/write/:id" element={<WritePost />} />
       </Routes>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useParams, Link } from "react-router-dom";
 
 const Home = () => {
+  const { id } = useParams();
+
   return (
-    <h1>
-        Home
-    </h1>
+    <div>
+      <h1>Home</h1>
+      <Link to={id ? `/read/${id}` : "login"}>Read</Link>
+      <Link to={id ? `/write/${id}` : "login"}>Write</Link>
+    </div>
   )
 }
 
